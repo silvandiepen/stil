5b96e70ff7abc5cdace323c36d95205d5b01a1bb
Sil van Diepen <me@sil.mt>
2020-12-11 14:35:25 +0100

    add styling to links in menu

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -5,9 +5,9 @@
 	<header class="header header--sticky background--white">
 		<a class="logo">Stil</a>
 		<nav class="navigation nav-bar">
-			<a class="navigation__link" href="#about">About</a>
-			<a class="navigation__link" href="#colors">Colors</a>
-			<a class="navigation__link" href="#grid">Grid</a>
+			<a class="navigation__link" href="#about"><span>About</span></a>
+			<a class="navigation__link" href="#colors"><span>Colors</span></a>
+			<a class="navigation__link" href="#grid"><span>Grid</span></a>
 		</nav>
 	</header>
 	<Content class="content" />
